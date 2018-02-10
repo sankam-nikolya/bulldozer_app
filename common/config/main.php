ab9dc114f9f76210c467b2185956c13a241e491b
Artem Kravchenko <lan143@lan143.ru>
2018-02-11 01:46:59 +0300

    Class fix

--- common/config/main.php
+++ common/config/main.php
@@ -3,7 +3,7 @@ return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'authManager' => [
-            'class' => 'bulldozer\users\rbac\DbManager',
+            'class' => \bulldozer\users\rbac\DbManager::class,
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
